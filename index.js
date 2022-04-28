d2ec5d2d4870128c8c767fb69f95ac2af91d6307
Carlos Vasquez <carlos.vasquez@enapsys.com>
2022-04-28 02:52:38 -0600

    fix

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-
+var jsonServer = require("json-server");
+var db = require("./db.json");
+var server = jsonServer.create();
+var router = jsonServer.router(db());
+var middlewares = jsonServer.defaults();
+var port = process.env.PORT || 5000;
 server.use(middlewares);
 server.use(router);
-const port = process.env.PORT || 6996;
-
-server.listen(port, () => {
-    console.log('JSON Server is running');
+server.listen(port, function () {
+    console.log("JSON Server is running on http://localhost:" + port);
 });
